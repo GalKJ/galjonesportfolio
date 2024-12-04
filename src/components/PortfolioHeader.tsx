42f6bd516d6f4ab9ced1b5aaa24307f6d525fcad
space doge <99536044+d0g3bl2@users.noreply.github.com>
2024-12-04 15:14:27 +0000

    Add PortfolioHeader component for portfolio display

---
+++ src/components/PortfolioHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PortfolioHeader() {
+  return (
+    <div className="z-10 max-w-5xl w-full justify-between font-mono lg:flex flex-col">
+      <h1 className="text-lightYellow font-bold text-4xl text-center">
+        Gal K Jones
+      </h1>
+      <h2 className="text-yellow text-xl text-center">
+        Junior Full Stack Application Developer
+      </h2>
+    </div>
+  );
+}
