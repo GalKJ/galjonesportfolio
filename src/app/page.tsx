8a02b43cdfd3e22bfdd428a6188c17dd1ce731fa
Gal Jones <gal.jones@citizenticket.co.uk>
2024-12-19 16:30:30 +0000

    Add Cv component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import LlmInterface from '@/components/LlmInterface';
-import PortfolioHeader from '@/components/PortfolioHeader';
-import React from 'react';
+import React from "react";
+import LlmInterface from "@/components/LlmInterface";
+import PortfolioHeader from "@/components/PortfolioHeader";
+import Cv from "@/components/Cv";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <PortfolioHeader />
+      <Cv />
       <LlmInterface />
     </main>
   );
