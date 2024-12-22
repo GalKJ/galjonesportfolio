26202baf01df25300331db4e18e1c50de2574b41
space doge <99536044+d0g3bl2@users.noreply.github.com>
2024-12-22 11:24:43 +0000

    Comment out LlmInterface component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import LlmInterface from "@/components/LlmInterface";
-import PortfolioHeader from "@/components/PortfolioHeader";
-import Cv from "@/components/Cv";
+import React from 'react';
+import LlmInterface from '@/components/LlmInterface';
+import PortfolioHeader from '@/components/PortfolioHeader';
+import Cv from '@/components/Cv';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <PortfolioHeader />
       <Cv />
-      <LlmInterface />
+      {/* <LlmInterface /> */}
     </main>
   );
 }
