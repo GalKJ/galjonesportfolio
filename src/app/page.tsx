1a5b160f9d4cfdcd53225832aea9cad873333fb9
space doge <99536044+d0g3bl2@users.noreply.github.com>
2024-12-25 11:25:35 +0000

    Update main layout padding for improved responsiveness and alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Cv from '@/components/Cv';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-4 md:p-8 lg:p-16 max-w-6xl mx-auto ">
       <PortfolioHeader />
       <Cv />
       {/* <LlmInterface /> */}
