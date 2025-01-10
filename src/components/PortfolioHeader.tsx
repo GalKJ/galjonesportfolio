03aae97d9e6768dc1634a3dc64ba698395a4d34a
space doge <99536044+d0g3bl2@users.noreply.github.com>
2025-01-10 18:20:31 +0000

    Add PDF download button to PortfolioHeader component

--- src/components/PortfolioHeader.tsx
+++ src/components/PortfolioHeader.tsx
@@ -1,8 +1,19 @@
+'use client';
 import React from 'react';
+import useGeneratePdf from '../hooks/useGeneratePdf';
 
 export default function PortfolioHeader() {
+  const generatePdf = useGeneratePdf();
   return (
     <div className="z-10 max-w-5xl w-full justify-between font-mono lg:flex flex-col">
+      {/* Download PDF Button */}
+      <button
+        onClick={generatePdf}
+        className="px-4 py-2 bg-gradient-to-r from-darkBrown to-yellow dark:text-lightYellow rounded w-40"
+      >
+        Download PDF
+      </button>
+
       <h1 className="text-lightYellow font-bold text-4xl text-center">
         Gal K Jones
       </h1>
