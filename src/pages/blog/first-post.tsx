8a3ae01ddb090b4dca80760f1f527b1d9aab9668
space doge <99536044+d0g3bl2@users.noreply.github.com>
2025-06-16 20:20:08 +0100

    fix: correct import style and update blog post content

--- src/pages/blog/first-post.tsx
+++ src/pages/blog/first-post.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
-export default function FirstPost() {
+export default function FirstPost(blogs) {
   return (
     <div>
       <h1>first post</h1>
+      <p>this will be the copy of the blog</p>
+      <p>{blogs}</p>
     </div>
   );
 }
