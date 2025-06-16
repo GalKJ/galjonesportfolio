cb6bfce9759b36f4678f4f66cff28e51e140e351
space doge <99536044+d0g3bl2@users.noreply.github.com>
2025-06-16 19:57:06 +0100

    Add Postcode Planner component with initial layout

---
+++ src/app/postcode-planner/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PostcodePlanner() {
+  return (
+    <div className="flex min-h-screen flex-col items-center p-4 md:p-8 lg:p-16 max-w-6xl mx-auto ">
+      <h1>Postcode Planner!</h1>
+    </div>
+  );
+}
